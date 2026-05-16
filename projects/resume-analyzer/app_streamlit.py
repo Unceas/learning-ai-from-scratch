efe7e4720d09d5687b77da62dd9ca03b7845f3dc
Unceas <Kayush2005k@gmail.com>
2026-05-16 20:52:42 +0530

    Day 86: Added PDF resume upload and automatic text extraction

--- projects/resume-analyzer/app_streamlit.py
+++ projects/resume-analyzer/app_streamlit.py
@@ -1,18 +1,50 @@
 import streamlit as st
 from analyzer import analyze_resume
+from PyPDF2 import PdfReader
 
+# ---------- EXTRACT PDF TEXT ----------
+def extract_text_from_pdf(pdf_file):
+
+    reader = PdfReader(pdf_file)
+
+    text = ""
+
+    for page in reader.pages:
+        extracted = page.extract_text()
+
+        if extracted:
+            text += extracted + " "
+
+    return text
+
+
+# ---------- UI ----------
 st.title("📄 AI Resume Analyzer")
 
-resume = st.text_area("Paste Resume Text")
+uploaded_file = st.file_uploader(
+    "Upload Resume (PDF)",
+    type=["pdf"]
+)
 
-if st.button("Analyze Resume"):
+if uploaded_file is not None:
 
-    result = analyze_resume(resume)
+    resume_text = extract_text_from_pdf(uploaded_file)
 
-    st.subheader(f"Resume Score: {result['score']}%")
+    st.subheader("Extracted Resume Text")
+    st.text_area(
+        "Resume Content",
+        resume_text,
+        height=250
+    )
 
-    st.write("### ✅ Skills Found")
-    st.write(result["found"])
+    if st.button("Analyze Resume"):
 
-    st.write("### ❌ Missing Skills")
-    st.write(result["missing"])
+        result = analyze_resume(resume_text)
+
+        st.subheader(f"Resume Score: {result['score']}%")
+
+        st.write("### ✅ Skills Found")
+        st.write(result["found"])
+
+        st.write("### ❌ Missing Skills")
+        st.write(result["missing"])
